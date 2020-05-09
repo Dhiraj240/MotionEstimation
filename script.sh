37fab93bdb4f7eed1860f377fb30632ae273a1cf
Dhiraj Sharma <dhiraj.sharma0024@gmail.com>
2020-05-09 22:04:33 +0530

    Updated alpine now

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 RUN apk update \
-    && apk install -y g++ \
-    && apk install -y cmake \
-    && apk install -y libgsf-1-dev \
-    && apk install -y libc++-dev \
-    && aapk install -y git \
+    && apk add g++ \
+    && apk add cmake \
+    && apk add libgsf-1-dev \
+    && apk add libc++-dev \
+    && apk add git \
     # This is a workaround to solve an issue with xlocale.h due it is not present in glibc 2.26
     #&& ln -s /usr/include/locale.h /usr/include/xlocale.h \
     && cd / \
@@ -14,6 +14,6 @@ RUN apk update \
     && cmake ../esajpip-SWHV/ -DCMAKE_INSTALL_PREFIX=$HOME/esajpip -DSWHV_PORT_JPIP=8090 -DSWHV_DIR_IMAGE=$HOME/esajpip/images -DSWHV_DIR_LOG=$HOME/esajpip/log \
     && make install \
     && mkdir $HOME/esajpip/{images,log} \
-    && cd /root/esajpip/server/esajpip/
+&& cd /root/esajpip/server/esajpip/
 
 top
